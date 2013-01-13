8d671f2c40e55701467bddac5e7540ac003086d0
Florian Flachsenberg <Flachsenberg@gmail.com>
2013-01-14 00:38:00 +0100

    ready

--- ue11/createmsp.c
+++ ue11/createmsp.c
@@ -7,6 +7,7 @@
 #define MAXLINE 1024
 #define STARTSIZE 16
 
+/* read hits from file in format [i,j] */
 int** read_hits (char* filename, int* length)
 {
   *length = 0;
@@ -50,33 +51,39 @@ int** read_hits (char* filename, int* length)
   return output;
 }
 
+/* extend hit to the left */
 int extendhitleft(char* u,int m,char* w,int n,int q,int match,int mismatch,int xd,int i,int j)
 {
   int xmax = 0;
   int scoresum = 0;
   int l = 0;
   int sc;
+  /* start at i-1 and j-1 */
   while (i-1-l >= 0 && j-1-l >= 0)
   {
+    /* scoring function */
     if (u[i-1-l] == w[j-1-l])
       sc = match;
     else
       sc = mismatch;
-      printf("%c", u[i-1-l]);
 
     scoresum += sc;
 
+    /* stop if scoresum is below threshold */
     if (scoresum < (xmax-xd))
       break;
+    /* check if new max-score */
     if (scoresum > xmax)
       xmax = scoresum;
 
+    //printf("%c", u[i-1-l]);
     l++;
   }
-  printf("\n");
+  //printf("\n");
   return l-1;
 }
 
+/* extend hit to the right */
 int extendhitright(char* u,int m,char* w,int n,int q,int match,int mismatch,int xd,int i,int j)
 {
   /*printf("control: match = %d, mismatch = %d, q = %d, xd = %d\n", match,
@@ -85,6 +92,7 @@ int extendhitright(char* u,int m,char* w,int n,int q,int match,int mismatch,int 
   int scoresum = 0;
   int r = 0;
   int sc;
+  /* start at i+q and j+q */
   while (i+q+r < m && j+q+r < n)
   {
     if (u[i+q+r] == w[j+q+r])
@@ -92,22 +100,58 @@ int extendhitright(char* u,int m,char* w,int n,int q,int match,int mismatch,int 
     else
       sc = mismatch;
 
-    printf("%c",u[i+q+r]);
-
     scoresum += sc;
 
+    /* check if scoresum is below threshold */
     if (scoresum < (xmax-xd))
     {
       break;
     }
+    /* check if new max score */
     if (scoresum > xmax)
     {
       xmax = scoresum;
     }
+    //printf("%c",u[i+q+r]);
     r++;
   }
-  printf("\n");
+  //printf("\n");
   return r-1;
+}
+
+/* extend hits to left and right */
+void extend(char* database, int m, char* query, int n, int q, int match, int mismatch, int xd, int** hits, int hitlength)
+{
+  int i,right,left,leftbd,leftbq,rightbd,rightbq,len;
+  for (i = 0; i < hitlength; i++)
+  {
+    /* calculate extensions to left and right */
+    left =
+      extendhitleft(database,m,query,n,q,match,mismatch,xd,hits[i][0],hits[i][1]);
+    right =
+      extendhitright(database,m,query,n,q,match,mismatch,xd,hits[i][0],hits[i][1]);
+    /* left boundaries: i-1-left and j-1-left */
+    leftbd = hits[i][0]-1-left;
+    leftbq = hits[i][1]-1-left;
+    /* right boundaries: i+q+right and j+q+right */
+    rightbd = hits[i][0]+right+q;
+    rightbq = hits[i][1]+right+q;
+    len = rightbd-leftbd+1;
+    printf("(%d-%d) (%d-%d) length %d\n",leftbd,rightbd,leftbq,rightbq,len);
+    /* print out results */
+    char* print = &(database[leftbd]);
+    char temp = database[rightbd+1];
+    database[rightbd+1] = '\0';
+    printf("%s\n",print);
+    database[rightbd+1] = temp;
+    print = &(query[leftbq]);
+    temp = query[rightbq+1];
+    query[rightbq+1] = '\0';
+    printf("%s\n",print);
+    query[rightbq+1] = temp;
+
+    printf("\n");
+  }
 }
 
 int main(int argc, char* argv[])
@@ -119,6 +163,7 @@ int main(int argc, char* argv[])
     return 0;
   }
 
+  /* read parameters from command line */
   int q,match,mismatch,xd;
   if (sscanf(argv[4],"%d",&q) != 1 || q <= 0)
   {
@@ -141,6 +186,7 @@ int main(int argc, char* argv[])
     return 1;
   }
 
+  /* read sequences from file */
   multifasta* mf1 = read_fasta_file(argv[1]);
   multifasta* mf2 = read_fasta_file(argv[2]);
 
@@ -152,40 +198,19 @@ int main(int argc, char* argv[])
   int m = strlen(database);
   int n = strlen(query);
 
+  /* read hits from file */
   int length;
   int** hits = read_hits(argv[3],&length);
   if (hits == NULL)
     return 1;
 
-  int i,right,left,leftbd,leftbq,rightbd,rightbq,len;
+  /* extend hits */
+  extend(database, m, query, n, q, match, mismatch, xd, hits, length);
+
+  int i;
   for (i = 0; i < length; i++)
-  {
-    left =
-      extendhitleft(database,m,query,n,q,match,mismatch,xd,hits[i][0],hits[i][1]);
-    right =
-      extendhitright(database,m,query,n,q,match,mismatch,xd,hits[i][0],hits[i][1]);
-    leftbd = hits[i][0]-1-left;
-    leftbq = hits[i][1]-1-left;
-    rightbd = hits[i][0]+right+q;
-    rightbq = hits[i][1]+right+q;
-    len = rightbd-leftbd+1;
-    printf("(%d-%d) (%d-%d) length %d\n",leftbd,rightbd,leftbq,rightbq,len);
-    char* print = &(database[leftbd]);
-    char temp = database[rightbd+1];
-    database[rightbd+1] = '\0';
-    printf("%s\n",print);
-    database[rightbd+1] = temp;
-    print = &(query[leftbq]);
-    temp = query[rightbq+1];
-    query[rightbq+1] = '\0';
-    printf("%s\n",print);
-    query[rightbq+1] = temp;
+    free(hits[i]);
 
-
-    printf("\n");
-  }
-
-  //printf("\n %c",database[333]);
   free(hits);
   multifasta_delete(mf1);
   multifasta_delete(mf2);
